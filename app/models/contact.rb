fe03b29b76b8d2fe30d60e2c1b31f96755788dbf
jailsonsf <jailson.s.f@outlook.com>
2023-03-28 12:23:46 -0300

    Feat: Add code from section 1 class 18

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+  belongs_to :kind
+
+  # def author
+  #   'Jailson Soares'
+  # end
+
+  # def kind_description
+  #   kind.description
+  # end
+
+  # def as_json(options={})
+  #   super(
+  #     methods: %i[kind_description author]
+  #   )
+  # end
 end
