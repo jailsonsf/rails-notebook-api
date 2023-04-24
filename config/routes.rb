55858a2ffc18886412feed38f15bc6fee1fdb575
jailsonsf <jailson.s.f@outlook.com>
2023-04-24 16:09:23 -0300

    Completed Active Model Serializers module

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   resources :contacts do
     resource :kind, only: %i[show]
     resource :kind, only: %i[show], path: 'relationships/kind'
+
+    resource :phones, only: %i[show]
+    resource :phones, only: %i[show], path: 'relationships/phones'
+
+    resource :phone, only: %i[update create destroy]
+    resource :phone, only: %i[update create destroy], path: 'relationships/phone'
+
+    resource :address, only: %i[show update create destroy]
+    resource :address, only: %i[show update create destroy], path: 'relationships/addr:address'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
